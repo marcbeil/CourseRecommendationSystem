90584ae694c5cddbc0d77abbedf5ef4c6d46860f
marcbeil <marcbeil2001@gmail.com>
2024-07-25 17:44:29 +0200

    filter modules

--- src/course_recommender/extraction_schema.py
+++ src/course_recommender/extraction_schema.py
@@ -1,23 +1,34 @@
-import enum
+from enum import Enum
 from typing import Optional, Set
 
 from langchain_core.pydantic_v1 import BaseModel, Field
 
+from course_recommender.db.models import Session, Organisation
+from db import models
 
-class Major(enum.Enum):
+
+class Major(Enum):
     COMPUTER_SCIENCE = "Computer Science"
     MATHEMATICS = "Mathematics"
     INFORMATION_SYSTEMS = "Information Systems"
     MEDICINE = "Medicine"
     ECONOMICS = "Economics"
 
+    @staticmethod
+    def values():
+        return [major.value for major in Major]
 
-class StudyLevel(enum.Enum):
+
+class StudyLevel(Enum):
     BACHELOR = "Bachelor"
     MASTER = "Master"
     DOCTOR = "Doctor"
     OTHER = "Other"
     UNKNOWN = "Unknown"
+
+    @staticmethod
+    def values():
+        return [level.value for level in StudyLevel]
 
     def get_level_filter(self):
         default_level = ["Unknown"]
@@ -30,7 +41,22 @@ class StudyLevel(enum.Enum):
                 return default_level
 
 
-class School(enum.Enum):
+# Retrieve all Departments from db
+session = Session()
+department_rows = (
+    session.query(Organisation.name)
+    .filter(Organisation.org_type == "department")
+    .distinct()
+    .all()
+)
+departments = [
+    (department[0].removeprefix("Department ").upper(), department[0])
+    for department in department_rows
+]
+Department = Enum("Department", departments)
+
+
+class School(Enum):
     COMPUTATION_INFORMATION_TECHNOLOGY = "Computation, Information and Technology"
     ENGINEERING_DESIGN = "Engineering and Design"
     NATURAL_SCIENCES = "Natural Sciences"
@@ -39,11 +65,28 @@ class School(enum.Enum):
     MANAGEMENT = "Management"
     SOCIAL_SCIENCES_TECHNOLOGY = "Social Sciences and Technology"
 
+    @staticmethod
+    def values():
+        return [school.value for school in School]
 
-class ModuleLanguage(enum.Enum):
-    ENGLISH = "English"
+
+class ModuleLanguage(Enum):
     GERMAN = "German"
+    ENGLISH = "English"
     OTHER = "Other"
+
+    @staticmethod
+    def values():
+        return [lang.value for lang in ModuleLanguage]
+
+    @staticmethod
+    def get_filters(languages: []):
+        default_filters = set("Unknown")
+        for lang in languages:
+            default_filters.add(lang.value)
+        if ModuleLanguage.GERMAN in languages and ModuleLanguage.ENGLISH in languages:
+            default_filters.add("German/English")
+        return default_filters
 
 
 class StudentPreferences(BaseModel):
@@ -60,8 +103,19 @@ class StudentPreferences(BaseModel):
     school: Optional[School] = Field(
         description="School the student is studying at. This is not the university. It is one level lower in the hierarchy. University -> School -> Department -> Chair. Assign the student to one of the schools in the examples if he hasn't specified a school. You can use the major to identify the school"
     )
+
+    departments: Set[Department] = Field(
+        description="Departments the student is interested in."
+    )
+
     semester: Optional[int] = Field(
         default=None, description="Current Semester of the student"
+    )
+    ects_min: Optional[int] = Field(
+        default=None, description="Minimum amount of ects for the module", gt=0
+    )
+    ects_max: Optional[int] = Field(
+        default=None, description="Maximum amount of ects for the module", gt=0
     )
     topics_of_interest: Set[str] = Field(
         default=None, description="The topics the student is interested in."
--- src/course_recommender/module_filter.py
+++ src/course_recommender/module_filter.py
@@ -1,19 +1,34 @@
-from sqlalchemy import create_engine, Table, Column, String, MetaData, select, cte, text
-from sqlalchemy.orm import aliased
-from sqlalchemy.sql import and_, not_
+from itertools import chain
+
+from sqlalchemy import (
+    create_engine,
+    Table,
+    Column,
+    String,
+    MetaData,
+    select,
+    cte,
+    text,
+    exists,
+)
+from sqlalchemy.sql import and_
+
+
 import logging
 
-from course_recommender.topic_mapper_new import (
-    _get_topics_with_embeddings,
-    build_index,
-    vector_similarity_search,
-)
+from course_recommender.topic_mapper import VectorStore
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-from course_recommender.extraction_schema import StudentPreferences, StudyLevel
+from course_recommender.extraction_schema import (
+    StudentPreferences,
+    StudyLevel,
+    ModuleLanguage,
+    Department,
+    School,
+)
 
 from db.models import Session, Module, Organisation, ModuleTopicMapping, Topic
 
@@ -25,25 +40,52 @@ def filter_modules(pref: StudentPreferences):
 
     log_template = "filter ({type}): {content}"
 
+    # TODO: fix language filter
     if pref.module_languages:
         logging.info(
             log_template.format(type="language", content=pref.module_languages)
         )
-        included_languages = [lang.value for lang in pref.module_languages]
+        included_languages = ModuleLanguage.get_filters(pref.module_languages)
         filters.append(Module.lang.in_(included_languages))
 
     if pref.study_level:
-        logging.info(
-            log_template.format(
-                type="level", content=pref.study_level.get_level_filter()
-            )
-        )
+        logging.info(log_template.format(type="level", content=pref.study_level))
         included_study_levels = pref.study_level.get_level_filter()
         filters.append(Module.level.in_(included_study_levels))
 
+    if pref.ects_min:
+        logging.info(log_template.format(type="ects-min", content=pref.ects_min))
+        filters.append(Module.ects >= pref.ects_min)
+
+    if pref.ects_max:
+        logging.info(log_template.format(type="ects-max", content=pref.ects_max))
+        filters.append(Module.ects <= pref.ects_max)
+
     if pref.school:
         logging.info(log_template.format(type="school", content=pref.school.value))
-        filters.append(_filter_for_school(session, school_name=pref.school.value))
+        # Retrieve the school_id based on the school name
+        school_id = (
+            session.query(Organisation.org_id)
+            .filter(Organisation.name == pref.school.value)
+            .first()[0]
+        )
+
+        # Join Modules with Organisations to filter by school_id
+        query = query.join(Organisation, Module.org_id == Organisation.org_id)
+        filters.append(Organisation.school_id == school_id)
+
+    if pref.departments:
+        logging.info(log_template.format(type="school", content=pref.departments))
+        if not pref.school:
+            query = query.join(Organisation, Module.org_id == Organisation.org_id)
+
+        department_rows = (
+            session.query(Organisation.org_id)
+            .filter(Organisation.name.in_([dep.value for dep in pref.departments]))
+            .all()
+        )
+        department_ids = map(department_rows, lambda row: row[0])
+        filters.append(Organisation.dep_id.in_(department_ids))
 
     if pref.previous_courses:
         logging.info(
@@ -55,53 +97,98 @@ def filter_modules(pref: StudentPreferences):
         query = query.filter(and_(*filters))
 
     filtered_modules = query.all()
+
+    if (pref.topics_of_interest and len(pref.topics_of_interest) > 0) or (
+        pref.topics_to_exclude and len(pref.topics_to_exclude) > 0
+    ):
+        module_ids = [f.module_id for f in filtered_modules]
+        topics = (
+            session.query(Topic)
+            .join(ModuleTopicMapping)
+            .filter(ModuleTopicMapping.module_id.in_(module_ids))
+            .distinct()
+            .all()
+        )
+        vector_store = VectorStore(topics=topics)
+        if pref.topics_of_interest and len(pref.topics_of_interest) > 0:
+            logging.info(
+                log_template.format(
+                    type="topics of interest", content=pref.topics_of_interest
+                )
+            )
+            mapped_topics = vector_store.map_topics(topics=pref.topics_of_interest, k=1)
+
+            print(mapped_topics)
+
+            # Flatten the list of lists of topics
+            all_topics = list(
+                chain.from_iterable([items[1] for items in mapped_topics.items()])
+            )
+
+            # Extract topic_ids from the flattened list of topics
+            topic_ids_to_filter = [topic.topic_id for topic in all_topics]
+
+            filtered_modules = (
+                session.query(Module)
+                .join(ModuleTopicMapping)
+                .filter(Module.module_id.in_(module_ids))
+                .filter(ModuleTopicMapping.topic_id.in_(topic_ids_to_filter))
+                .all()
+            )
+
+        if pref.topics_to_exclude and len(pref.topics_to_exclude) > 0:
+            logging.info(
+                log_template.format(
+                    type="excluded topics", content=pref.topics_to_exclude
+                )
+            )
+            mapped_topics = vector_store.map_topics(topics=pref.topics_to_exclude, k=1)
+
+            print(mapped_topics)
+
+            # Flatten the list of lists of topics
+            all_topics = list(
+                chain.from_iterable([items[1] for items in mapped_topics.items()])
+            )
+
+            # Extract topic_ids from the flattened list of topics
+            topic_ids_to_filter = [topic.topic_id for topic in all_topics]
+            module_ids = [f.module_id for f in filtered_modules]
+            print(topic_ids_to_filter)
+            print(module_ids)
+            subquery = select(ModuleTopicMapping).where(
+                and_(
+                    ModuleTopicMapping.module_id == Module.module_id,
+                    ModuleTopicMapping.topic_id.in_(topic_ids_to_filter),
+                )
+            )
+            print(filtered_modules)
+            # Main query
+            filtered_modules_query = (
+                session.query(Module)
+                .distinct()
+                .filter(Module.module_id.in_(module_ids))
+                .filter(~exists(subquery))
+            )
+            filtered_modules = filtered_modules_query.all()
+
     session.close()
     return filtered_modules
-
-
-def _get_all_organisations_for_school(session, school_name: str):
-    school_id = (
-        session.query(Organisation.org_id)
-        .filter(Organisation.name == school_name)
-        .filter(Organisation.org_type == "school")
-        .scalar()
-    )
-    # Using a recursive CTE to filter modules based on school hierarchy
-    org_hierarchy = aliased(Organisation, name="org_hierarchy")
-    # anchor
-    subquery = (
-        session.query(org_hierarchy.org_id)
-        .filter(org_hierarchy.org_id == school_id)
-        .cte(recursive=True)
-    )
-    # recursive member
-    filtered_organisations = subquery.union_all(
-        session.query(org_hierarchy.org_id).join(
-            subquery, org_hierarchy.parent_org_id == subquery.c.org_id
-        )
-    )
-
-    return filtered_organisations
-
-
-def _filter_for_school(session, school_name: str):
-    school_organisation_ids = _get_all_organisations_for_school(
-        session, school_name=school_name
-    )
-    school_filter = Module.org_id.in_(select(school_organisation_ids))
-    return school_filter
 
 
 # Example usage
 pref = StudentPreferences(
     # module_languages={"English"},
     # study_level="Bachelor",
-    school="Engineering and Design",
+    school=School.COMPUTATION_INFORMATION_TECHNOLOGY,
+    departments=[Department.MATHEMATICS],
     # previous_courses={"Math 101", "CS 201"},
-    topics_of_interest=["machine learning"],
+    topics_of_interest=["Dikstra Algorithm"],
+    topics_to_exclude=["Affine space"],
 )
 filtered_modules = filter_modules(pref)
 
 print(len(filtered_modules))
+print(filtered_modules)
 
 # print([m for m in filtered_modules])
